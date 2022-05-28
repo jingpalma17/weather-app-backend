e9a929ec117370dc31f374fc711e1593c0837427
Jing Palma <jingpalma17@gmail.com>
2022-05-29 00:57:34 +0800

    Implemented some test route for checking env value on prod

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -6,17 +6,25 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { WeatherService } from './weather.service';
+import { ConfigService } from '@nestjs/config';
 
-@ApiOAuth2([])
-@ApiBearerAuth('access-token')
+// @ApiOAuth2([])
+// @ApiBearerAuth('access-token')
 @ApiTags('Weather')
 @Controller('weather')
 export class WeatherController {
-  constructor(private readonly weatherService: WeatherService) {}
+  constructor(private readonly weatherService: WeatherService,
+    private configService: ConfigService,) {}
 
   @Get('')
   @ApiOperation({ summary: 'Get weather' })
   async getWeather(): Promise<any> {
     return this.weatherService.getWeather();
+  }
+
+  @Get('test')
+  getTest(): string {
+    console.log(this.configService.get('WEATHER_API_BASE_URL'));
+    return '';
   }
 }
