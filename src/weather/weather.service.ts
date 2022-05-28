3bba05b2283eca1f7b1814c6013041948e70a961
Jing Palma <jingpalma17@gmail.com>
2022-05-29 00:33:45 +0800

    Implemented weather service and initial function for fetching the data from third party api

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpService } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AxiosError } from 'axios';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  constructor(
+    private httpService: HttpService,
+    private configService: ConfigService,
+  ) {}
+
+  async getWeather(): Promise<any> {
+    const weatherData = await new Promise(async (resolve, reject) => {
+      this.httpService
+        .get(
+          `${this.configService.get(
+            'WEATHER_API_BASE_URL',
+          )}weather?q=${'Manila'},philippines&appid=${this.configService.get(
+            'WEATHER_API_KEY',
+          )}`,
+        )
+        .subscribe(
+          async (response) => {
+            resolve(response.data);
+          },
+          async (error) => {
+            const newError = this.getHttpError(error, 'GET /weather');
+
+            reject(newError);
+          },
+        );
+    });
+    return weatherData;
+  }
+
+  // TODO Moved to the utils herper folder for axios error
+  getHttpError(error: AxiosError, message: string): HttpException {
+    if (error.response) {
+      return new HttpException(
+        {
+          statusCode: error.response.status,
+          message: `${message} ${error.response.statusText}`,
+        },
+        error.response.status,
+      );
+    }
+    if (error.request) {
+      return new HttpException(
+        {
+          statusCode: HttpStatus.REQUEST_TIMEOUT,
+          message: `${message} No Response`,
+        },
+        HttpStatus.REQUEST_TIMEOUT,
+      );
+    }
+
+    return new HttpException(
+      {
+        statusCode: HttpStatus.AMBIGUOUS,
+        message: `${message} ${error.message}`,
+      },
+      HttpStatus.AMBIGUOUS,
+    );
+  }
+}
